89d894b3e18d5970e7330b6c86db666368d2b26d
Michael Hegglin <mhegglinmichael@gmail.com>
2017-05-30 15:16:45 -0700

    first commit

---
+++ app/src/main/java/com/example/mike/sendbroadcast/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.mike.sendbroadcast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendOutBroadcast(View v) {
+        //creates an intent
+        Intent intent = new Intent();
+        //in basic terms creating a key for this broadcast to differ from others
+        intent.setAction("com.example.mike.sendbroadcast");
+        //making sure the broadcast is compatible
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        //sends the broadcast with the method
+        sendBroadcast(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
